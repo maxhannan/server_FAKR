e4279e37ebaadbccf4c7c03e90fc481cea3c4b4b
maxhannan <maxhannan@substanceseven.com>
2021-04-21 15:01:38 -0500

    auth setup with passport

--- server.js
+++ server.js
@@ -1,10 +1,58 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
-
+const { MONGODB } = require('./config');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
 const PORT = 4000;
 
 const app = express();
 const path = '/gql';
+// MIDDLEWARE
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(
+  session({
+    secret: 'maxiboy',
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        '351314765576-uf1ofncpvb1g5191pp7pkbblfrl7cvml.apps.googleusercontent.com',
+      clientSecret: '4tRgMXZJ9NudaD7X_rUiWUBl',
+      callbackURL: '/auth/google/callback',
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      cb(null, profile);
+    },
+  ),
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }),
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  },
+);
+
+// APOLLO SERVER STUFF
 const typeDefs = gql`
   type Query {
     hello: String
@@ -17,13 +65,18 @@ const resolvers = {
   },
 };
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
-
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path });
 
-app.listen({ port: PORT }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
-);
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Mongo Connected');
+  })
+  .then(() => {
+    app.listen({ port: PORT }, () =>
+      console.log(
+        `🚀 GQL Server ready at http://localhost:4000${server.graphqlPath} `,
+      ),
+    );
+  });
